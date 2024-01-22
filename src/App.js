4970a964ee0e54a880c8a5d4ba651af23c59f556
abhishek9125 <abhishek4075@gmail.com>
2024-01-22 23:54:14 +0530

    Route Setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Explorer from './components/Explorer';
+import Pagination from './components/Pagination';
+
 
 function App() {
 	
 
 	return (
-		<div className="App">
-
-		</div>
+		<BrowserRouter>
+			<div className="App">
+				<Routes>
+					<Route path="/explorer" element={<Explorer />}  />
+					<Route path="/pagination" element={<Pagination />}  />
+				</Routes>
+			</div>
+		</BrowserRouter>
 	);
 }
 

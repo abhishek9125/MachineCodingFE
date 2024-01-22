7534db0809513c665e414bfb1eaafbf34acd4e0d
abhishek9125 <abhishek4075@gmail.com>
2024-01-22 23:45:15 +0530

    Coding Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import InfiniteScroll from './components/InfiniteScroll';
 
 function App() {
+	
+
 	return (
 		<div className="App">
-			<InfiniteScroll />
+
 		</div>
 	);
 }
